e40584b7af71b1ac3c1a9fb9de0d00763d87870d
Steven <steven@steven-home-serv.(none)>
2014-04-03 19:46:02 -0400

    Demo

---
+++ gobelin_demo/settings.js
@@ -0,0 +1,10 @@
+module.exports = {
+  port      : 8082,
+  db  : {
+    url     : "localhost" ,
+    port    : "27017"     ,
+    name    : "truc" ,
+    user    : "admin"     ,
+    password: "truc"   
+  }
+}
