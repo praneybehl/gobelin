e295fee5b687919c56fc758efeeb690d7b5ea685
Steven <steven@steven-home-serv.(none)>
2014-04-03 19:44:16 -0400

    Controllers + base server

---
+++ src/lib/gobelin.js
@@ -0,0 +1,51 @@
+// Use commander to parse arguments
+var program = require('commander')
+var colors = require('colors')
+var util = require('util')
+var express = require('express');
+var getRoutes = require('../lib/getRoutes').getRoutes
+var settings
+program.version('0.1.1')
+  .option('-s, --settings <settings>', 'Settings used to launch server. If empty, uses "settings.js" from current working directory.')
+  .option('-l, --location <location>', 'Folder where the project is situated. If empty, uses the current working directory.')
+  .option('-p, --port <n>', 'Override the port in settings. Not recommended.', parseInt)
+  .parse(process.argv);
+
+
+local = {
+  run : function(){
+    app = express()
+    getRoutes(function(){
+      var server = app.listen(port, function() {
+        console.log('\nGobelin is running with the following settings : '.bold)
+        console.log('Port : '.bold + port.toString().green)
+        console.log('Database : '.bold + util.format("%s@%s/%s:%s", settings.db.user, settings.db.url, settings.db.name, settings.db.port).green)
+        console.log(util.format("Press %s to shut down the server.\n".bold, "CTRL+C".magenta))
+      })
+    })
+  },
+}
+
+// Main
+module.exports = {
+  initialize : function (){
+    if(!program.args[0])
+      program.args[0] = "run"
+    if(program.settings)
+      settings = require(util.format('%s/%s',process.cwd(), program.settings))
+    else
+      settings = require(util.format('%s/%s',process.cwd(), "settings.js"))   
+    if(program.location){
+      location = program.location
+      if(location[location.length - 1]!='/')
+        location += "/"      
+    }else{
+      location = process.cwd() + "/"
+    }
+    if(program.port)
+      port = program.port
+    else
+      port = settings.port
+    local[program.args[0]]() 
+  }
+}
