8a69c3df1c7c56730a5bf420bf19be96c9fdc7b2
Steven <steven@steven-home-serv.(none)>
2014-04-05 12:50:28 -0400

    Finish models

--- src/lib/getModules.js
+++ src/lib/getModules.js
@@ -2,6 +2,7 @@ var fs = require('fs'),
     path = require('path'),
     util = require('util'),
     mongoose = require('mongoose')
+
 // function used to load all controllers
 function loadControllers(file){
   verbs = ["all","get","post","head","put","delete"]
@@ -12,18 +13,24 @@ function loadControllers(file){
       controller = require(pathToController)
       Object.keys(controller).forEach(function (key) {
         var func = controller[key];
-        app[verb](util.format('/%s/%s',module,key), func)
+        if(key.toLowerCase() == "index")
+          route = util.format('/%s',module)
+        else
+          route = util.format('/%s/%s',module,key)
+        app[verb](route , func)
       }) 
     } 
   })
 }
 
+// function used to load and expose models
 function loadModels(file){
   pathToModel = util.format("%s/%s",file, "models.js")
   if (fs.existsSync(pathToModel)) {
-    console.log("-- Loading models")
     model = require(pathToModel)
-    Object.keys(model).forEach(function (key) {
+    modelKeys = Object.keys(model)
+    console.log(util.format("-- Loading %s models",modelKeys.length.toString().bold))
+    modelKeys.forEach(function (key) {
       models[key] = mongoose.model(key, model[key])
     })
   }
